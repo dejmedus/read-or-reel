8a017ca3e5a0ace126a299f3854f5f0103eea3e3
FaithKauwe <fkauwe@gmail.com>
2025-02-12 11:27:45 -0800

    books rendering in browser

--- app/components/book-card.tsx
+++ app/components/book-card.tsx
@@ -5,28 +5,29 @@ export default function BookCard({ bookData }: { bookData: IBook }) {
   const {
     id,
     title,
-    author,
-    description,
-    goodreadsRating,
-    yearPublished,
-    genre,
     imageUrl,
   } = bookData;
 
   console.log(
     id,
     title,
-    author,
-    description,
-    goodreadsRating,
-    yearPublished,
-    genre,
+   
     imageUrl
   );
 
   return (
-    <Link className="hover:font-bold underline" to={`/books/${id}`}>
-      <div>this is a card for {title} book</div>
+    <Link to={`/books/${id}`} className="block">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img 
+          src={imageUrl} 
+          alt={`Cover of ${title}`} 
+          className="w-full h-64 object-cover"
+        />
+        <div className="px-4 py-2">
+          <h2 className="font-bold text-xl">{title}</h2>
+        </div>
+      </div>
     </Link>
   );
+
 }
